66b61cd01cd010891baf20dc104af062b1336b77
rabia <rabia.olker@gmail.com>
2022-09-30 14:04:11 +0300

    A favorites file has been created with a card to be shown when the favorite is a starship and an alert message to be shown otherwise

---
+++ src/Components/pages/Favourites/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
+
+import Card from "Components/pages/Starships/card";
+
+const Favourite = () => {
+  const favorites = useSelector((state) => state.starships.favorites);
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        {favorites &&
+          favorites.map((favorite) => (
+            <Card key={favorite.url} starship={favorite} />
+          ))}
+
+        {favorites.length < 1 && (
+          <div className="text-center text-muted fs-4 my-5">
+            <p>There is no favorite starship.</p>
+          </div>
+        )}
+
+        <div className="d-flex justify-content-center mt-5 ">
+          <Link
+            to="/starships"
+            className="back-page text-muted text-decoration-none  d-flex align-items-center"
+          >
+            <FontAwesomeIcon icon={faCaretLeft} className="fa-2x" />
+            <span>Back to Starships Page</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Favourite;
