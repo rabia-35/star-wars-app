da3df0a2499fbd7486802fbc67e610d705a36b50
rabia <rabia.olker@gmail.com>
2022-09-30 14:52:08 +0300

    When rendering, all data up to 4 pages were pulled with fetchStarship and 6 data were displayed each time when load more was pressed.

---
+++ src/Components/pages/Starships/starships.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+import Card from "./card";
+import { fetchStarship, backStarships } from "Redux/starWars/starWarsSlice";
+import Search from "Components/pages/Starships/search";
+import Loading from "Components/Loading";
+import Error from "Components/Error";
+
+const Starships = () => {
+  const starships = useSelector((state) => state.starships.items);
+  const status = useSelector((state) => state.starships.status);
+
+  // const filteredStarship = useSelector(
+  //   (state) => state.starships.filteredStarship
+  // );
+
+  const [page, setPage] = useState(6);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    for (let i = 1; i < 5; i++) {
+      dispatch(fetchStarship(i));
+    }
+  }, [dispatch]);
+
+  const handleClick = () => {
+    setPage(page + 6);
+  };
+
+  const scrollWin = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <Search />
+      </div>
+      <div className="row pb-5">
+        {starships && (
+          <>
+            {starships.map(
+              (starship, i) =>
+                i < page && <Card key={starship.url} starship={starship} />
+            )}
+
+            {starships && page < 36 && (
+              <div className="text-center">
+                <button
+                  className="btn btn-lg  btn-outline-warning mt-5"
+                  onClick={handleClick}
+                >
+                  Load More
+                </button>
+              </div>
+            )}
+          </>
+        )}
+        <div
+          role="button"
+          className="text-muted text-center mt-3"
+          onClick={scrollWin}
+        >
+          <FontAwesomeIcon icon={faChevronUp} className="fa-2x me-2" />
+          <p>Başa Dön</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Starships;
