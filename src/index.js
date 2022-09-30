e462d80f5baf62c72c71d23e003b14fa4ea5b1eb
rabia <rabia.olker@gmail.com>
2022-09-30 13:11:39 +0300

    removed React.StrictMode and added Provider and store for redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "index.css";
+import App from "App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { store } from "Redux/store";
+import { Provider } from "react-redux";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
