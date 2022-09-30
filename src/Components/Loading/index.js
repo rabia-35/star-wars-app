990ebcf78ce2daa9b72887ad5aa5fba76f67ee59
rabia <rabia.olker@gmail.com>
2022-09-30 15:18:12 +0300

    Removed text-light to get default color in app file and added margin-top value

--- src/Components/Loading/index.js
+++ src/Components/Loading/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="text-center text-light">
+    <div className="text-center mt-5">
       <div
         className="spinner-border"
         style={{ width: "4rem", height: "4rem" }}
