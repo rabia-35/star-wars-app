b433d6d0346a078256c0c0d8502dbb0dd514dc66
rabia <rabia.olker@gmail.com>
2022-09-30 13:10:08 +0300

    Router with paths and components has been added to the App file whose background color changes according to the mode state.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "Components/pages/Home";
+import Favourites from "Components/pages/Favourites";
+import Detail from "Components/pages/Detail";
+import Starships from "Components/pages/Starships/starships";
 import "./App.css";
+import Navbar from "Components/Navbar/navbar";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div></div>;
+  const mode = useSelector((state) => state.starships.mode);
+
+  let bgColor = "";
+  let color = "";
+  if (!mode) {
+    bgColor = "#00263d";
+    color = "white";
+  } else {
+    bgColor = "#ffffff";
+    color = "black";
+  }
+  return (
+    <Router>
+      <div className="App" style={{ backgroundColor: bgColor, color: color }}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/favourites/:number" element={<Favourites />} />
+          <Route path="/starships/:name" element={<Detail />} />
+          <Route exact path="/starships" element={<Starships />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
