488ea7a6ec4441e0677c857843547a7075773e1d
rabia <rabia.olker@gmail.com>
2022-10-28 20:17:28 +0300

    Fixed filenames

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "index.css";
 import App from "App";
 
-import { store } from "Redux/store";
+import { store } from "redux/store";
 import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
