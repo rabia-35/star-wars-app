677e89bc387d548299f71487354dd98cbe17e249
rabia <rabia.olker@gmail.com>
2023-02-11 17:17:02 +0300

    Routes pulled from AppRouter file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useSelector } from "react-redux";
-
-import Home from "pages/Home";
-import Favourites from "pages/Favourites";
-import Detail from "pages/Detail";
-import Starships from "pages/StarshipsList";
 import "./App.css";
 import Navbar from "components/Navbar";
-import Page404 from "pages/Page404";
 import Footer from "components/Footer";
+import AppRouter from "routes";
 
 function App() {
   const mode = useSelector((state) => state.starships.mode);
-  const page = useSelector((state) => state.starships.page);
-  const [position, setPosition] = useState(false);
+
   let bgColor = "";
   let color = "";
   if (!mode) {
@@ -24,24 +18,13 @@ function App() {
     bgColor = "#ffffff";
     color = "black";
   }
-  console.log(page);
+
   return (
     <Router>
       <div className="App" style={{ backgroundColor: bgColor, color: color }}>
         <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/favourites/:number" element={<Favourites />} />
-          <Route path="/starships/:name" element={<Detail />} />
-          <Route exact path="/starships" element={<Starships />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-
-        {page > 1 && (
-          <>
-            <Footer />
-          </>
-        )}
+        <AppRouter />
+        <Footer />
       </div>
     </Router>
   );
