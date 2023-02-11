8814f135d8d70a6ca1320312ba6f4e5ba7381113
rabia <rabia.olker@gmail.com>
2023-02-11 17:20:28 +0300

    Fields related to card info have been deleted from this component

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,112 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./style.css";
-import { Link, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faTrashCan } from "@fortawesome/free-solid-svg-icons";
-
-import { addFavorites, removeFavorites } from "redux/StarWarsSlice";
+import {useParams } from "react-router-dom";
+import CardInfo from "./cardInfo";
 
 const Card = ({ starship }) => {
   const { number } = useParams();
-  const favorites = useSelector((state) => state.starships.favorites);
-  const [onStar, setOnStar] = useState();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (onStar) {
-      dispatch(addFavorites(starship));
-    } else if (onStar === false) {
-      dispatch(removeFavorites(starship.url));
-    }
-  }, [onStar]);
-
-  const handleClick = () => {
-    onStar ? setOnStar(false) : setOnStar(true);
-  };
-
-  const removeFavoriteClick = () => {
-    setOnStar(false);
-  };
-  const onFavorite = favorites.every((item) => item.url !== starship.url);
 
   const detailName = starship.name.match(/\w+/g).join("-");
 
   return (
     <div
-      className=" col-12 col-sm-6 col-lg-4  mx-auto "
+      className=" col-12 col-sm-6 col-lg-4"
       onClick={() => sessionStorage.setItem("position", window.scrollY)}
     >
       <div className="card mb-3">
-        <div className="row g-0">
-          {/********** Link to detail page   ***********/}
-          <Link
-            to={`/starships/${detailName}`}
-            className="col-md-6 img-background text-center py-4 px-md-"
-          >
-            <img
-              src={`../images/starships/${detailName}.png`}
-              className="img-fluid rounded-start"
-              alt={detailName}
-            />
-          </Link>
-
-          <div className="col-md-6 text-dark">
-            <div className="card-body">
-              <div className="card-info">
-                <h1 className="card-title fs-6">{starship.name}</h1>
-                <p className="card-text  mb-lg-4">
-                  <strong>Model </strong>
-                  <span className="ms-2 fst-italic mb-5-lg">
-                    {starship.model}
-                  </span>
-                </p>
-                <p className="card-text text-muted">
-                  <strong>Hyperdrive Rating </strong>
-                  <span className="fst-italic">
-                    {starship.hyperdrive_rating}
-                  </span>
-                </p>
-              </div>
-
-              <div className="d-flex justify-content-between">
-                {/**** Icon to show when number is not present  ****/}
-                {!number && (
-                  <div onClick={handleClick} role="button">
-                    <FontAwesomeIcon
-                      icon={faStar}
-                      className={
-                        onStar || !onFavorite
-                          ? "text-warning"
-                          : "text-secondary"
-                      }
-                    />
-                  </div>
-                )}
-
-                {number && (
-                  <div
-                    role="button"
-                    className="text-secondary"
-                    onClick={removeFavoriteClick}
-                  >
-                    <FontAwesomeIcon
-                      icon={faTrashCan}
-                      className="trash-icon fs-5"
-                    />
-                  </div>
-                )}
-
-                <Link
-                  to={`/starships/${detailName}`}
-                  className="btn btn-outline-warning "
-                >
-                  Detail
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <img
+          src={`../images/starships/${detailName}.png`}
+          className="img-fluid rounded-start"
+          alt={detailName}
+        />
+        <h1 className="card-title fs-6">{starship.name}</h1>
+          <CardInfo
+            number={number}
+            starship={starship}
+            detailName={detailName}
+          />
       </div>
     </div>
   );
