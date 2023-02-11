f26ccba538b6432635e3831a02488f94cb79253c
rabia <rabia.olker@gmail.com>
2023-02-11 17:25:55 +0300

    Added autocomplete feature

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchStarship } from "redux/StarWarsSlice";
-
+import './style.css';
+import dataTitles from './titles.json'
 const Search = () => {
   const [text, setText] = useState("");
-  const error = useSelector((state) => state.starships.error);
+  const [filteredTitle, setFilteredTitle]=useState([])
+  const error = useSelector((state) => state.starships.error) 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (text.length > 2) {
-      dispatch(searchStarship(text));
+    const handleChange=(e)=>{
+      setText(e.target.value.replace(/\s+/," "))
     }
-  }, [dispatch, text]);
+
+  useEffect(() => {
+    if (text.length >1) {
+      setFilteredTitle(dataTitles.titles.filter(item=> item.title.toLowerCase().includes(text.toLowerCase()) ))
+    }else{setFilteredTitle([])}
+
+  }, [text]);
+
 
   return (
     <>
-      <div className="d-flex justify-content-center">
-        <form className="input-group  mb-3" onSubmit={() => setText("")}>
-          <input
-            type="search"
-            className="form-control"
-            placeholder="Enter Name or Model"
-            aria-label="Search"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </form>
+      <div className="search-area">
+        <form className="input-group  mb-3" onSubmit={(e)=> e.preventDefault()}>
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Enter Name or Model"
+              aria-label="Search"
+              value={text}
+              onChange={handleChange}
+            />
+          </form>
+              <ul>
+                {filteredTitle.map((item,ind)=>(
+                  <li key={ind} onClick={()=>{dispatch(searchStarship(item.title.toLowerCase())); setText("")}} ><a href="#" >{item.title}</a></li>
+                ))}
+              </ul>
       </div>
 
       {error.length > 0 && (
